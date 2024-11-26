291069d2c98b4b5ad1145ca3fd957787cfe2c69b
leorvergani <vergani@outlook.com.br>
2024-11-25 21:42:00 -0300

    obj simple

---
+++ index.ts
@@ -0,0 +1,25 @@
+// Objeto Simples
+
+let usuario: { nome: string; idade: number, senha: string, email: string, ativo: boolean } = {
+    nome: "Carlos",
+    idade: 30,
+    senha: "1234",
+    email: "verga@vergas.com.br",
+    ativo: true
+};
+
+// exercicios
+
+const livro: {
+    titulo: string,
+    autor: string,
+    anoPublicacao: number,
+    categorias: string[],
+    disponivel: boolean,
+} = {
+    titulo: "Livro 1888",
+    autor: "Não sei",
+    anoPublicacao: 1888,
+    categorias: ["Ficção"],
+    disponivel: true
+}
